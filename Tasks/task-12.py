0ea585566ba6c6160a12a929ae170701d1485a87
Name idlogush <ivan.dlogush-ext@jci.com>
2020-11-09 22:25:24 +0200

    task 12 refactoring

--- Tasks/task-12.py
+++ Tasks/task-12.py
@@ -3,19 +3,30 @@
 a = [1, 3, 5, 7, 9]
 b = [0, 2, 4, 6, 8, 10, 12, 14]
 
+# c = []
+# i = j = 0
+# while i < len(a) or j < len(b):
+#     if i > len(a) - 1:
+#         c.append(b[j])
+#         j += 1
+#     elif j > len(b) - 1:
+#         c.append(a[i])
+#         i += 1
+#     elif a[i] < b[j]:
+#         c.append(a[i])
+#         i += 1
+#     else:
+#         c.append(b[j])
+#         j += 1
+# print(c)
+
 c = []
 i = j = 0
 while i < len(a) or j < len(b):
-    if i > len(a) - 1:
+    if i > len(a) - 1 or a[i] > b[j]:
         c.append(b[j])
         j += 1
-    elif j > len(b) - 1:
+    elif j > len(b) - 1 or a[i] < b[j]:
         c.append(a[i])
         i += 1
-    elif a[i] < b[j]:
-        c.append(a[i])
-        i += 1
-    else:
-        c.append(b[j])
-        j += 1
 print(c)
