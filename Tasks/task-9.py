465e4b4eb64a0d168017e8f4d8c5b731793909bf
Name idlogush <ivan.dlogush-ext@jci.com>
2020-11-09 22:08:31 +0200

    task 9 refactoring

--- Tasks/task-9.py
+++ Tasks/task-9.py
@@ -1,7 +1,10 @@
 # 9 Прибавить к элементам массива их индекс.
 
-# a = [6, -8, 1, -20, 48, 3, -1, -0, -8, 15]
-a = [0, 1, 2, 3, 4, 5]
-for i in range(len(a)):
-    a[i] += i
-print(a)
+# a = [0, 1, 2, 3, 4, 5]
+# for i in range(len(a)):
+#     a[i] += i
+# print(a)
+
+b = [0, 1, 2, 3, 4, 5]
+res = [b[i] + i for i in range(len(b))]
+print(res)
