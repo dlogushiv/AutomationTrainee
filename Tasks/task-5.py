71d93a973dd99002767903ed7d9552a024907e6e
Name idlogush <ivan.dlogush-ext@jci.com>
2020-11-09 18:06:34 +0200

    task 5 refactoring

--- Tasks/task-5.py
+++ Tasks/task-5.py
@@ -1,5 +1,12 @@
 # 5 Найти сумму элементов массива.
 
-a = [1, 2, 3, 4, 5]
-result = sum(a)
+# a = [1, 2, 3, 4, 5]
+# result = sum(a)
+# print(result)
+
+
+a = [1, 2, 3, 4, 5, 6]
+result = 0
+for el in a:
+    result += el
 print(result)
