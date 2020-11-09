220fbd2556b5ed673f7c64b61b76a2f82ead3224
Name idlogush <ivan.dlogush-ext@jci.com>
2020-11-09 22:04:49 +0200

    task 8 refactoring

--- Tasks/task-8.py
+++ Tasks/task-8.py
@@ -1,8 +1,11 @@
 # 8 Посчитать количество элементов больше нуля.
 
 a = [6, -8, 1, -20, 48, 3, -1, -0, -8, 15]
-result = 0
-for el in a:
-    if el < 0:
-        result += 1
+# result = 0
+# for el in a:
+#     if el > 0:
+#         result += 1
+
+res = len([i for i in a if i > 0])
 print(result)
+print(res)
