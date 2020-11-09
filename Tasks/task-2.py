3ef82f6016a4c78aeb60eb8ef2a2ad77ab2fe053
Name idlogush <ivan.dlogush-ext@jci.com>
2020-11-09 17:51:11 +0200

    task 2 refactoring

--- Tasks/task-2.py
+++ Tasks/task-2.py
@@ -1,18 +1,32 @@
 # 2. Получить список всех чисел Фибоначчи на отрезке [a, b].
 
-def fibo(x):
-    c = [0, 1]
-    i = c[0]
-    j = c[1]
-    while j <= x - i:
-        fib = i + j
-        i, j = j, fib
-        c.append(fib)
-    return c
+# def fibo(x):
+#     c = [0, 1]
+#     i = c[0]
+#     j = c[1]
+#     while j <= x - i:
+#         fib = i + j
+#         i, j = j, fib
+#         c.append(fib)
+#     return c
+#
+#
+# a = int(input())
+# b = int(input())
+#
+# result = [i for i in fibo(b) if i >= a]
+# print(result)
 
 
-a = int(input())
-b = int(input())
+def fibo(start, stop):
+    res = []
+    i = 0
+    j = 1
+    while i <= stop:
+        if i >= start:
+            res.append(i)
+        i, j = j, i + j
+    return res
 
-result = [i for i in fibo(b) if i >= a]
-print(result)
+
+print(fibo(10, 100))
