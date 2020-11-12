bb8461e12c19712be8bd89985c5bfb43bdb9acea
dlogushiv <dlogushiv@gmail.com>
2020-11-12 13:55:15 +0200

    added all files for run test in TMC

--- TMC_Tests/test.py
+++ TMC_Tests/test.py
@@ -1,26 +1,15 @@
-import os, sys
-import time
+# import os
+# import sys
+# import time
+# import datetime
 
-import datetime
-
-from atl.utils.sequences.namespace import Namespace
 from autolib.neo import NeoTestCase, Data
-from autolib.pmax import TestList, TestRunner, LoadTestsFromClass
-from testcase.testcase import TestCase
 from testcase.testflow import executable
 
 _ALIAS_ = 'Testcases'
 
-
-class Data(Namespace):
-    def __init__(self): pass
-
-
 @executable(context=Data)
 class Trainee(NeoTestCase):
-    def __init__(self, data, test):
-        self.data = data
-        super().__init__(test)
 
     def CreateEnvironment(self): pass
 
